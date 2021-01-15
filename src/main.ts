ea4ae2369dbd42ed7fb5e78487b9adaaa58180d3
luis <le.yanessantiago@gmail.com>
2021-01-15 02:02:20 -0500

    Adding database seed.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { PrismaClient } from '@prisma/client';
 import { AppModule } from './app.module';
 
+const contacts = require('../prisma/contacts.json');
+
+const prisma = new PrismaClient();
+
 async function bootstrap() {
+  try {
+    const contact = await prisma.contact.findFirst();
+    if (!contact) {
+      console.log(contact);
+      await Promise.all(
+        contacts.map(async contact => {
+          return await prisma.contact.create({ data: { ...contact } });
+        })
+      );
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
 }
